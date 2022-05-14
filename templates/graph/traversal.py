0e30130478a75b4c529c6450229a9f4147ec17ab
JaeminSon <woalsdnd@gmail.com>
2022-05-15 01:26:36 +0900

    [bug fix] dfs_postorder

--- templates/graph/traversal.py
+++ templates/graph/traversal.py
@@ -3,6 +3,7 @@ from collections import deque
 
 
 def dfs_preorder(adjacency_list, root, neighbor_func):
+    # returns valid preorder sequence
     stack = [root]
     marked = set([root])
     preorder = []
@@ -16,20 +17,33 @@ def dfs_preorder(adjacency_list, root, neighbor_func):
     return preorder
 
 def dfs_postorder(adjacency_list, root, neighbor_func):
+    # returns valid postorder sequence
     stack = [root]
+    second_visit = set()
     marked = set([root])
-    postorder = deque()
+    postorder = []
     while stack:
         node = stack.pop()
-        postorder.appendleft(node)
-        for ne in neighbor_func(adjacency_list, node):
-            if ne not in marked:
-                stack.append(ne)
-                marked.add(ne)
-    return list(postorder)
+        list_neighbors = [ne for ne in neighbor_func(adjacency_list, node) if ne not in marked]
+        if len(list_neighbors)==0: # add leaf node
+            postorder.append(node)
+        else:
+            if node in second_visit: # add second visit of the intermediate node
+                postorder.append(node)
+            else:
+                stack.append(node) # push node at first visit of the intermediate node
+                # push neighbors
+                for ne in list_neighbors:
+                    if ne not in marked:
+                        stack.append(ne)
+                        marked.add(ne)
+                second_visit.add(node)
+                
+    return postorder
 
 
 def bfs(adjacency_list, root, neighbor_func):
+    # returns valid bfs sequence
     q = deque([root])
     marked = set([root])
     order = []
@@ -75,8 +89,17 @@ if __name__=="__main__":
     adjacency_list = [[1, 2], [0, 3, 4], [0, 5, 6], [1], [1, 5], [2, 4], [2]]
     neighbor_func = lambda x,y:x[y]
     print(dfs_preorder(adjacency_list, 0, neighbor_func)) # [0, 2, 6, 5, 4, 1, 3]
-    print(dfs_postorder(adjacency_list, 0, neighbor_func)) # [3, 1, 4, 5, 6, 2, 0]
+    print(dfs_postorder(adjacency_list, 0, neighbor_func)) # [6, 4, 5, 2, 3, 1, 0]
     print(bfs(adjacency_list, 0, neighbor_func)) # [0, 1, 2, 3, 4, 5, 6]
+
+    ###################
+    # graph structure #
+    ######   0   ######
+    ####  1     2 #####
+    ###  3 4 - 5 6 #### (node 4-5 and 3-6 are connected)
+    ##   |_______|   ##
+    ###################
+    print(dfs_postorder([[1, 2], [0, 3, 4], [0, 5, 6], [1,6], [1, 5], [2, 4], [2, 3]], 0, neighbor_func)) # [3, 6, 4, 5, 2, 1, 0]
 
     ##################
     # tree structure #
