a671cdb063179363d8317ca248fda3bafe9cd97e
JaeminSon <woalsdnd@gmail.com>
2022-05-15 22:33:50 +0900

    add algorithms for shortest path (single node or all pairs) and minimum spanning tree (kruskal, prim).py

---
+++ templates/graph/minimum_spanning_tree.py
@@ -0,0 +1,63 @@
+def kruskal_mst(adjacency_list):
+
+    def find(parent:list, i:int):
+        if parent[i] == i:
+            return i
+        return find(parent, parent[i])
+ 
+    def union(parent, rank, x, y):
+        xroot = find(parent, x)
+        yroot = find(parent, y)
+        
+        # subsume to higher rank
+        if rank[xroot] < rank[yroot]:
+            parent[xroot] = yroot
+        elif rank[xroot] > rank[yroot]:
+            parent[yroot] = xroot
+        else: # tie-break (promote xroot)
+            parent[yroot] = xroot
+            rank[xroot] += 1
+
+    # sort edges
+    edges = [(s,t,w) for s, e in enumerate(adjacency_list) for t, w in e]
+    edges.sort(key=lambda x:x[2])
+
+    # union-find data structure
+    n_nodes = len(adjacency_list)
+    parent = [node for node in range(n_nodes)]
+    rank = [0] * n_nodes
+
+    # greedily add edges
+    mst = []
+    for s, t, w in edges:
+        if find(parent, s) != find(parent, t):
+            mst.append((s, t, w))
+            union(parent, rank, s, t)
+
+    return mst
+ 
+import heapq
+def prim_mst(adjacency_list):
+    
+    start_node = min([(s,t,w) for s, e in enumerate(adjacency_list) for t, w in e], key=lambda x:x[2])[0]
+    visited = set([start_node])
+    edges = [(cost, start_node, to) for to, cost in adjacency_list[start_node]]
+    heapq.heapify(edges)
+    
+    mst = []
+    while edges:
+        cost, s, t = heapq.heappop(edges)
+        if t not in visited:
+            visited.add(t)
+            mst.append((s,t, cost))
+            for next, cost in adjacency_list[t]:
+                if next not in visited:
+                    heapq.heappush(edges, (cost, t, next))
+
+    return mst
+ 
+if __name__ == "__main__":
+    print(kruskal_mst([[(1,10),(2,6),(3,5)],[(0,10),(3,15)],[(0,6),(3,4)],[(0,5),(2,4)]])) # [(2, 3, 4), (0, 3, 5), (0, 1, 10)]
+    print(prim_mst([[(1,10),(2,6),(3,5)],[(0,10),(3,15)],[(0,6),(3,4)],[(0,5),(2,4)]])) #[(2, 3, 4), (3, 0, 5), (0, 1, 10)]
+    print(kruskal_mst([[(1,2),(3,6)],[(0,2),(2,3),(3,8),(4,5)],[(1,3),(3,7)],[(0,6),(1,8),(4,9)],[(1,5),(2,7),(3,9)]])) # [(0, 1, 2), (1, 2, 3), (1, 4, 5), (0, 3, 6)]
+    print(prim_mst([[(1,2),(3,6)],[(0,2),(2,3),(3,8),(4,5)],[(1,3),(3,7)],[(0,6),(1,8),(4,9)],[(1,5),(2,7),(3,9)]])) # [(0, 1, 2), (1, 2, 3), (1, 4, 5), (0, 3, 6)]
---
+++ templates/graph/shortest_path.py
@@ -0,0 +1,86 @@
+import heapq
+def dijkstra(adjacency_list, s, t):
+    
+    
+    n_nodes = len(adjacency_list)
+    distance = [float("Inf")] * n_nodes
+    distance[s] = 0
+    visited = set([s])
+    prev = {}
+    
+    pq = [(0, s)]
+    heapq.heapify(pq)
+    while pq:
+        cost, curr = heapq.heappop(pq)
+        visited.add(curr)
+        for next, cost in adjacency_list[curr]:
+            if next not in visited and distance[next] > distance[curr] + cost: 
+                distance[next] = distance[curr] + cost
+                heapq.heappush(pq, (distance[next], next))
+                prev[next] = curr
+
+    def get_trajectory(node:int):
+        if node != s:
+            return get_trajectory(prev[node]) + [node]
+        else:
+            return [node]
+    
+    return get_trajectory(t), distance[t]
+ 
+def bellman_ford(adjacency_list, s):
+     
+    n_nodes = len(adjacency_list)
+    dist = [float("Inf")] * n_nodes
+    dist[s] = 0
+
+    for _ in range(n_nodes - 1):
+        for u in range(n_nodes):
+            for v, w in adjacency_list[u]:
+                if dist[u] != float("Inf") and dist[u] + w < dist[v]:
+                    dist[v] = dist[u] + w
+
+    # check negative cycle
+    for u in range(n_nodes):
+        for v, w in adjacency_list[u]:
+            if dist[u] != float("Inf") and dist[u] + w < dist[v]:
+                return None # distance has no meaning with negative cycle
+    
+    return dist
+
+
+def floyd_warshall(adjacency_list):
+   
+    n_nodes = len(adjacency_list)
+
+    dist = [[float("Inf")]*n_nodes for _ in range(n_nodes)]
+    for i in range(n_nodes):
+        dist[i][i]=0
+    for s in range(n_nodes):
+        for t, w in adjacency_list[s]:
+            dist[s][t] = w
+ 
+    for k in range(n_nodes):
+        for i in range(n_nodes):
+            for j in range(n_nodes):
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+ 
+    return dist
+    
+if __name__ == "__main__":
+    for t in range(9):
+        print(dijkstra([[(1,4),(7,8)], [(0,4),(2,8),(7,11)], [(1,8),(3,7),(5,4),(8,2)], [(2,7),(4,9),(5,14)], [(3,9),(5,10)], [(2,4),(3,14),(4,10),(6,2)], [(5,2),(7,1),(8,6)], [(0,8),(1,11),(6,1),(8,7)], [(2,2),(6,6),(7,7)]], 0, t))
+    """
+    ([0], 0)
+    ([0, 1], 4)
+    ([0, 1, 2], 12)
+    ([0, 1, 2, 3], 19)
+    ([0, 7, 6, 5, 4], 21)
+    ([0, 7, 6, 5], 11)
+    ([0, 7, 6], 9)
+    ([0, 7], 8)
+    ([0, 1, 2, 8], 14)
+    """ 
+    
+    print(bellman_ford([[(1,-1),(2,4)],[(2,3),(3,2),(4,2)], [], [(2,5),(1,1)],[(3,-3)]], 0)) # [0, -1, 2, -2, 1]
+    print(floyd_warshall([[(1,5),(3,10)], [(2,3)], [(3,1)], []])) # [[0, 5, 8, 9], [inf, 0, 3, 4], [inf, inf, 0, 1], [inf, inf, inf, 0]]
+    
