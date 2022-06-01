8958027ed2108a24a6fd2a220385ffe508f6672f
JaeminSon <woalsdnd@gmail.com>
2022-06-02 07:53:00 +0900

    add convex hull trick

--- templates/optimization/dynamic_programming.py
+++ templates/optimization/dynamic_programming.py
@@ -53,6 +53,59 @@ def bitonic_traveling_salesman(distance_matrix):
     
     return dp[0][0]
 
+
+def convex_hull_trick(A, B, C, D):
+    # dp[i] = min(0 ≤ j < i){A[i]B[j] + dp[j]+ C[j]} + D[i] 
+    # constraint: 1. B is monotonically decreasing => O(nlogn)
+    #             2. A is monotonically increasing => O(n)
+    # line segment formula: f(x) = B[j] * x + dp[j]+ C[j] (A[i] ~ x)
+ 
+    from bisect import bisect
+    
+    assert len(A) == len(B)
+    if C:
+        assert len(A) == len(C)
+    if D:
+        assert len(A) == len(D)
+    
+    def intersection_x(lseg1, lseg2):
+        # y=ax+b, (x>=s)
+        a1,b1,_ = lseg1
+        a2,b2,_ = lseg2
+        return 1.*(b2-b1)/(a1-a2)
+ 
+    n = len(A)
+    dp = [0] * n
+    stack = [] # (a,b,s) where y=ax+b, x>=s
+    pos = 0
+    for i in range(1, n):
+        line_seg = [B[i-1], dp[i-1] + C[i-1] if C else dp[i-1], 0]
+        while stack:
+            line_seg[2] = intersection_x(stack[-1], line_seg)
+            if stack[-1][2] < line_seg[2]:
+                break
+            else:
+                stack.pop()
+        stack.append(line_seg)
+ 
+        # # A is not monotonically increasing (binary search)
+        # lo, hi = 0, len(stack)
+        # while lo < hi:
+        #     mid = (lo + hi) // 2
+        #     if stack[mid][2] < A[i]:
+        #         lo = mid + 1
+        #     else:
+        #         hi = mid
+        # dp[i] = stack[lo-1][0] * A[i] + stack[lo-1][1]
+        
+        # A is monotonically increasing
+        while pos+1 < len(stack) and stack[pos+1][2] < A[i]:
+            pos+=1
+        dp[i] = stack[pos][0] * A[i] + stack[pos][1]
+ 
+    return dp[n-1]
+
+
 if __name__ == "__main__":
     X = "AGGTAB"
     Y = "GXTXAYB"
@@ -75,3 +128,6 @@ if __name__ == "__main__":
         return distance_matrix
  
     print(bitonic_traveling_salesman(points2distance_matrix([(1,1),(2,3),(3,1)])))
+
+    print(convex_hull_trick(A=[1,2,3,4,5],B=[5,4,3,2,0], C=None, D=None))
+    print(convex_hull_trick(A=[1,2,3,10,20,30],B=[6,5,4,3,2,0], C=None, D=None))
