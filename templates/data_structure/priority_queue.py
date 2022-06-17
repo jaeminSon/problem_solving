8ddc6296d6d90197fdd122355d4686da7769b82f
JaeminSon <woalsdnd@gmail.com>
2022-06-17 21:57:57 +0900

    add comments and CAVEATE for heapq

--- templates/data_structure/priority_queue.py
+++ templates/data_structure/priority_queue.py
@@ -5,7 +5,9 @@ heappush(h, (5, 'write code'))
 heappush(h, (7, 'release product'))
 heappush(h, (1, 'write spec'))
 heappush(h, (3, 'create tests'))
-assert heappop(h) == (1, 'write spec')
+heappush(h, (1, 'create tests'))
+assert heappop(h) == (1, 'create tests')
+assert heappop(h) == (1, 'write spec') # compare with first elements and then second and so on
 
 
 from queue import PriorityQueue
