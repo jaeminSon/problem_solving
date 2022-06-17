06de8714ef9a8318c486af5f9edffb5239f4b20d
JaeminSon <woalsdnd@gmail.com>
2022-06-17 21:58:11 +0900

    add merge_k_sorted_lists.py

---
+++ leetcode/merge_k_sorted_lists.py
@@ -0,0 +1,51 @@
+from typing import List, Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+
+        if len(lists) == 0:
+            return None
+        elif all([l is None for l in lists]):
+            return None
+
+        from heapq import heappush, heappop
+
+        front = []
+        for i, node in enumerate(lists):
+            if node is not None:
+                heappush(front, (node.val, i, node))
+                lists[i] = lists[i].next
+            
+        prev = None
+        while front:
+            _, li, next = heappop(front)
+            if lists[li] is not None:
+                heappush(front, (lists[li].val, li, lists[li]))
+                lists[li] = lists[li].next
+
+            if prev is None:
+                start = next
+                prev = next
+            else:
+                prev.next = next
+                prev = next
+            
+        return start
+
+
+a = ListNode(1)
+b = ListNode(4)
+c = ListNode(5)
+a.next=b
+b.next=c
+
+l = ListNode(2)
+m = ListNode(6)
+l.next=m
+print(Solution().mergeKLists([a,None,l]))
+
