6992a5eb44cf3c040c5f7602d5d42d77948ddfd6
JaeminSon <woalsdnd@gmail.com>
2022-06-17 21:17:28 +0900

    add priority queue python libraries

---
+++ templates/data_structure/priority_queue.py
@@ -0,0 +1,17 @@
+from heapq import heappush, heappop
+
+h = []
+heappush(h, (5, 'write code'))
+heappush(h, (7, 'release product'))
+heappush(h, (1, 'write spec'))
+heappush(h, (3, 'create tests'))
+assert heappop(h) == (1, 'write spec')
+
+
+from queue import PriorityQueue
+que = PriorityQueue()
+que.put(4)
+que.put(1)
+que.put(7)
+que.put(3)
+assert que.get() == 1
