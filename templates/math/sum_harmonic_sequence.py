53412f9b0c02c5f0e97e4f0da849b51a2faf1568
JaeminSon <woalsdnd@gmail.com>
2022-06-26 16:51:18 +0900

    add sum of harmonic sequence

---
+++ templates/math/sum_harmonic_sequence.py
@@ -0,0 +1,12 @@
+    
+def sum_harmonic_seq(n):
+    # compute n//1 + n//2 + ... + n//n
+    s = 0
+    i=1
+    while i<=n:
+        j = n//(n//i)
+        s+=(n//i)*(j-i+1)
+        i = j+1
+    return s
+
+assert sum_harmonic_seq(16)==50
