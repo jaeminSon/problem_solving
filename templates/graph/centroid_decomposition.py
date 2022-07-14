b0147dcf80d3e45c4242afe525833cc4a76b6166
JaeminSon <woalsdnd@gmail.com>
2022-07-14 23:08:13 +0900

    add loop version of centroid decomposition

--- templates/graph/centroid_decomposition.py
+++ templates/graph/centroid_decomposition.py
@@ -1,4 +1,4 @@
-def get_centroid(adjacency_list:list):
+def get_centroid_recursive(adjacency_list:list):
 
     def get_size(node, parent):
         list_size[node] = 1
@@ -19,6 +19,46 @@ def get_centroid(adjacency_list:list):
     return _recursive(0,-1)
 
 
+def get_centroid_loop(adjacency_list:list):
+
+    def get_size(root):
+        stack = [root]
+        visited = set()
+        marked = set([root])
+        while stack:
+            node = stack.pop()
+            list_neighbors = [ne for ne in adjacency_list[node] if ne not in marked]
+            if len(list_neighbors)==0:
+                if node in visited: # intermediate node
+                    list_size[node] = 1 + sum([list_size[ne] for ne in adjacency_list[node]])
+                else: # leaf node
+                    list_size[node] = 1
+            else:
+                stack.append(node) # push node at first visit of the intermediate node
+                # push neighbors
+                for ne in list_neighbors:
+                    if ne not in marked:
+                        stack.append(ne)
+                        marked.add(ne)
+                visited.add(node)
+                    
+    def find_centroid(root):
+        stack = [root]
+        marked = set([root])
+        while stack:
+            node = stack.pop()
+            for ne in adjacency_list[node]:
+                if ne not in marked and list_size[ne]*2 > n_nodes:
+                    marked.add(ne)
+                    stack.append(ne)
+        return node
+
+    n_nodes = len(adjacency_list)
+    list_size = [0]*n_nodes
+    get_size(0)
+    return find_centroid(0)
+
+
 if __name__ == "__main__":
     ###################
     # tree structure #
@@ -27,4 +67,5 @@ if __name__ == "__main__":
     ###  3 4   5 6 ####
     ###################
     adjacency_list = [[1, 2], [0, 3, 4], [0, 5, 6], [1], [1], [2], [2]]
-    assert get_centroid(adjacency_list) == 0
+    assert get_centroid_recursive(adjacency_list) == 0
+    assert get_centroid_loop(adjacency_list) == 0
