431596c1d8fbb977e8d08b72e27a3029f433c401
jaeminSon <woalsdnd@gmail.com>
2023-01-08 19:36:28 +0900

    [pass test] 트리의외심.py

--- baekjoon/트리의외심.py
+++ baekjoon/트리의외심.py
@@ -18,54 +18,44 @@ Q = int(sys.stdin.readline().rstrip())
 problems = [(int(d)-1 for d in sys.stdin.readline().rstrip().split()) for _ in range(Q)]
 
 n_nodes = len(adjacency_list)
-tree = {"size":[None] * n_nodes, "parent":[None] * n_nodes, "depth":[None] * n_nodes, "chain_top":[None] * n_nodes}
+max_height = 20
 
+dp_parent = [[0]*n_nodes for _ in range(max_height)]
+depth = [0]*n_nodes
+visit = [False]*n_nodes
 
-def dfs(curr, parent, depth):
-    tree["parent"][curr] = parent
-    tree["depth"][curr] = depth
-    tree["size"][curr] = 1
-    for v in adjacency_list[curr]:
-        if v!=parent:
-            tree["size"][curr] += dfs(v, curr, depth+1)
-    return tree["size"][curr]
+def dfs(curr, parent, d):
+    visit[curr] = True
+    dp_parent[0][curr]=parent
+    depth[curr]=d
+    for ch in adjacency_list[curr]:
+        if not visit[ch]:
+            dfs(ch, curr, d+1)
     
-def _assign_chain_top(curr, parent, chain_top):
-    tree["chain_top"][curr] = chain_top
-    list_child = [v for v in adjacency_list[curr] if v!=parent]
-    if len(list_child) > 0: # intermediate node
-        node_heavy_edge = max(list_child, key=lambda x:tree["size"][x])
-        _assign_chain_top(node_heavy_edge, curr, chain_top) # remain top for heavy edge
-        for v in adjacency_list[curr]:
-            if v!=parent and v!=node_heavy_edge:
-                _assign_chain_top(v, curr, v) # light edge (new top)
-    
-dfs(0,-1,0)
-_assign_chain_top(0,-1,0)
+def go_up(node, dist):
+    for i in range(max_height, -1,-1):
+        if dist & (1<<i):
+            node = dp_parent[i][node]
+    return node
 
-def lca_query(u, v):
-    # iterate until the same chain
-    while tree["chain_top"][u] != tree["chain_top"][v]:
-        if tree["depth"][tree["chain_top"][u]] < tree["depth"][tree["chain_top"][v]]:
-            v = tree["parent"][tree["chain_top"][v]]
-        else:
-            u = tree["parent"][tree["chain_top"][u]]
+def lca_query(node1, node2):
+    node1=go_up(node1, max(0, depth[node1]-depth[node2]))
+    node2=go_up(node2, max(0, depth[node2]-depth[node1]))
+    if node1==node2:
+        return node1
+    else:
+        for i in range(max_height-1, -1, -1):
+            if dp_parent[i][node1] != dp_parent[i][node2]:
+                node1 = dp_parent[i][node1]
+                node2 = dp_parent[i][node2]
+        return dp_parent[0][node1]
 
-    return u if tree["depth"][u] < tree["depth"][v] else v # return node with lower-level
+dfs(0, -1, 0)
 
-
-def go_up(s, d):
-    for _ in range(d):
-        for e in backward_adj[s]:
-            s = e
-    return s
-
-# remain backward edges
-backward_adj = defaultdict(dict)
-for s in adjacency_list:
-    for e in adjacency_list[s]:
-        if tree["depth"][s] > tree["depth"][e]:
-            backward_adj[s].update({e:1})
+# preprocess
+for i in range(1, max_height):
+    for j in range(n_nodes):
+        dp_parent[i][j]=dp_parent[i-1][dp_parent[i-1][j]]
 
 # solve
 for p in problems:
@@ -75,28 +65,28 @@ for p in problems:
     lca_ca = lca_query(c,a)
     
     if lca_ab == lca_bc == lca_ca:
-        depths = sorted([tree["depth"][a],tree["depth"][b],tree["depth"][c]])
+        depths = sorted([depth[a],depth[b],depth[c]])
         if depths[0] == depths[1] and (depths[2]-depths[0]) % 2 == 0:
-            d = depths[2] - tree["depth"][lca_ab] - (depths[2] - depths[0])//2
-            deepest_node = max([a,b,c], key=lambda x: tree["depth"][x])
+            d = depths[2] - depth[lca_ab] - (depths[2] - depths[0])//2
+            deepest_node = max([a,b,c], key=lambda x: depth[x])
             print(go_up(deepest_node, d)+1)
         else:
             print(-1)
     else:
         lca_top = lca_query(lca_ab, c)
-        if tree["depth"][lca_ab] == tree["depth"][lca_ca] < tree["depth"][lca_bc]:
-            l_dist = [tree["depth"][b]-tree["depth"][lca_bc], tree["depth"][c]-tree["depth"][lca_bc], tree["depth"][lca_bc]-tree["depth"][lca_top]+tree["depth"][a]-tree["depth"][lca_top]]
-        elif tree["depth"][lca_ab] == tree["depth"][lca_bc] < tree["depth"][lca_ca]:
-            l_dist = [tree["depth"][c]-tree["depth"][lca_ca], tree["depth"][a]-tree["depth"][lca_ca], tree["depth"][lca_ca]-tree["depth"][lca_top]+tree["depth"][b]-tree["depth"][lca_top]]
-        elif tree["depth"][lca_ca] == tree["depth"][lca_bc] < tree["depth"][lca_ab]:
-            l_dist = [tree["depth"][a]-tree["depth"][lca_ab], tree["depth"][b]-tree["depth"][lca_ab], tree["depth"][lca_ab]-tree["depth"][lca_top]+tree["depth"][c]-tree["depth"][lca_top]]
+        if depth[lca_ab] == depth[lca_ca] < depth[lca_bc]:
+            l_dist = [depth[b]-depth[lca_bc], depth[c]-depth[lca_bc], depth[lca_bc]-depth[lca_top]+depth[a]-depth[lca_top]]
+        elif depth[lca_ab] == depth[lca_bc] < depth[lca_ca]:
+            l_dist = [depth[c]-depth[lca_ca], depth[a]-depth[lca_ca], depth[lca_ca]-depth[lca_top]+depth[b]-depth[lca_top]]
+        elif depth[lca_ca] == depth[lca_bc] < depth[lca_ab]:
+            l_dist = [depth[a]-depth[lca_ab], depth[b]-depth[lca_ab], depth[lca_ab]-depth[lca_top]+depth[c]-depth[lca_top]]
         else:
             raise ValueError("lca infeasible.")
 
         l_dist = sorted(l_dist)
         if l_dist[0] == l_dist[1] and (l_dist[2]-l_dist[0]) % 2==0:
             d = l_dist[0] + (l_dist[2]-l_dist[0])//2
-            deepest_node = max([a,b,c], key=lambda x: tree["depth"][x])
+            deepest_node = max([a,b,c], key=lambda x: depth[x])
             print(go_up(deepest_node, d)+1)
         else:
             print(-1)
