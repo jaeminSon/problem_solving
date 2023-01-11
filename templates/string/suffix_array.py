91375776cea6503b209e460eb9067674af4ba03b
jaeminSon <woalsdnd@gmail.com>
2023-01-11 21:17:13 +0900

    [refactor] modify comments

--- templates/string/suffix_array.py
+++ templates/string/suffix_array.py
@@ -60,24 +60,19 @@ def suffix_array(A):
     # na                    4
     # nana                  2
     """
-    # This implements the algorithm of Vladu and Negruşeri; see
-    # http://web.stanford.edu/class/cs97si/suffix-array.pdf
-
+    # algorithm: page 5 in http://web.stanford.edu/class/cs97si/suffix-array.pdf
+    
+    # P: order of A[i,i+count] in substrings the length of count
+    # L: ((P[i],P[i+count]) , start of substring)
+    
     L = sorted((a, i) for i, a in enumerate(A))
     n = len(A)
     count = 1
     while count < n:
-        # Invariant: L is now a list of pairs such that L[i][1] is the
-        # starting position in A of the i'th substring of length
-        # 'count' in sorted order. (Where we imagine A to be extended
-        # with dummy elements as necessary.)
 
         P = [0] * n
         for (r, i), (s, j) in zip(L, islice(L, 1, None)):
             P[j] = P[i] + (r != s)
-
-        # Invariant: P[i] is now the position of A[i:i+count] in the
-        # sorted list of unique substrings of A of length 'count'.
 
         L = sorted(chain((((P[i],  P[i+count]), i) for i in range(n - count)),
                          (((P[i], -1), i) for i in range(n - count, n))))
