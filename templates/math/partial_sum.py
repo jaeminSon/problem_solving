767293b63dcb895bedef64478fa4057a0eeaf7df
jaeminSon <woalsdnd@gmail.com>
2023-01-27 20:06:30 +0900

    add partial sum functions

---
+++ templates/math/partial_sum.py
@@ -0,0 +1,21 @@
+def cumulative_sum(l):
+    # s[i] = sum(l[:i])
+    s = [0]
+    for i in range(len(l)):
+        s.append(s[-1]+l[i])
+    return s
+
+
+def partial_sum(l):
+    # ps[i][j] = sum([i:j])
+    s = cumulative_sum(l)
+    n = len(l)+1
+    ps = [[0]*n for _ in range(n)]
+    for i in range(n):
+        for j in range(i+1, n):
+            ps[i][j] = s[j] - s[i]
+    return ps
+
+if __name__ == "__main__":
+    assert cumulative_sum([1,2,3])==[0,1,3,6]
+    assert partial_sum([1,2,3])==[[0,1,3,6],[0,0,2,5], [0,0,0,3],[0,0,0,0]]
