ca75f667c42421aec83f5eb80bdfaa6d50dfa25e
jaeminSon <woalsdnd@gmail.com>
2023-02-02 22:38:36 +0900

    add sqrt_decomposition for finding maximum non-zero element

--- templates/data_structure/sqrt_decomposition.py
+++ templates/data_structure/sqrt_decomposition.py
@@ -4,7 +4,7 @@ class SqrtDecomposition:
         self.n = len(arr)
         self.arr = arr
         self.block_size = int(self.n**(1./2))
-        self.block = [0] * self.n
+        self.block = [0] * (self.block_size + 1)
     
         # O(n)
         for i in range(self.n):
@@ -35,7 +35,32 @@ class SqrtDecomposition:
             l += 1
         
         return sum
-        
+
+
+class SqrtDecompositionSearchNonZero:
+    
+    def __init__(self, arr) -> None:
+        self.n = len(arr)
+        self.arr = arr # distribution
+        self.block_size = int(self.n**(1./2))
+        self.block = [0] * (self.block_size+1)
+    
+        # O(n)
+        for i in range(self.n):
+            self.block[i // self.block_size] += self.arr[i]
+  
+    def increment(self, idx, val):
+        # O(1)
+        self.block[idx // self.block_size] += val
+        self.arr[idx] += val
+    
+    def query(self):
+        # O(sqrt(n))
+        for i in range(len(self.block)-1, -1, -1):
+            if self.block[i] > 0:
+                for j in range(self.block_size, -1, -1):
+                    if self.arr[i*self.block_size + j] > 0:
+                        return i*self.block_size + j
         
 if __name__=="__main__":
 
@@ -45,3 +70,8 @@ if __name__=="__main__":
     assert sqrt_decomposition.query(1, 6) == 21
     sqrt_decomposition.update(8, 0)
     assert sqrt_decomposition.query(8, 8)==0
+
+    sqrt_decomposition_nonzero = SqrtDecompositionSearchNonZero([1, 0, 1, 0, 0, 0, 0, 0, 0, 0])
+    sqrt_decomposition_nonzero.query() == 2
+    sqrt_decomposition_nonzero.increment(3, 4)
+    sqrt_decomposition_nonzero.query() == 3
