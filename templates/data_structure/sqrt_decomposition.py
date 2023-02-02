aab0a66b63279d79c6c6a1fce9431c5ebd17c462
jaeminSon <woalsdnd@gmail.com>
2023-02-03 00:24:32 +0900

    [bug fix] prevent index out of range

--- templates/data_structure/sqrt_decomposition.py
+++ templates/data_structure/sqrt_decomposition.py
@@ -59,7 +59,7 @@ class SqrtDecompositionSearchNonZero:
         for i in range(len(self.block)-1, -1, -1):
             if self.block[i] > 0:
                 for j in range(self.block_size-1, -1, -1):
-                    if self.arr[i*self.block_size + j] > 0:
+                    if i*self.block_size + j < len(self.arr) and self.arr[i*self.block_size + j] > 0:
                         return i*self.block_size + j
         
 if __name__=="__main__":
