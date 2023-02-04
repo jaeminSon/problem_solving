09f3cae13dece8996459695859657de0a703a924
jaeminSon <woalsdnd@gmail.com>
2023-02-04 23:12:08 +0900

    [feature] add template for parallel binary search

--- templates/optimization/binary_search.py
+++ templates/optimization/binary_search.py
@@ -21,6 +21,35 @@ def binary_search_right_inclusive(list_val, query):
             hi = mid - 1
     return lo # find lo such that list_val[:lo] < query
 
+def parallel_binary_search(min_val, max_val, n_queries) : 
+    
+    l = [min_val] * n_queries
+    r = [max_val] * n_queries
+    ans = [None] * n_queries
+
+    while True:
+        # group queries by mid point
+        mid2query = {[] for _ in range(min_val, max_val+1)}
+        finished = True
+        for i in range(len(n_queries)):
+            if l[i] <= r[i]:  # binary search of [l,r]
+            # if l[i] < r[i]:  # binary search of [l,r)
+                finished = False
+                mid2query[(l[i] + r[i]) // 2].append(i)
+        if finished:
+            break
+
+        # run algorithm and handle each query
+        for i in range(None): # TODO set for-loop
+            op_count = i + 1
+            # TODO fill some operation here
+            for index_query in mid2query[i+1]:
+                if None: # TODO set hit-condition
+                    ans[index_query] = None # TODO set answer
+                    r[index_query] = op_count - 1 # binary search of [l,r]
+                    # r[index_query] = op_count # binary search of [l,r)
+                else:
+                    l[index_query] = op_count + 1
 
 import bisect
 list_val = [-1,0,1,2,4,5,6]
