469b8aea6a547e7a5671a2e7f8294b0d4dda22da
jaeminSon <woalsdnd@gmail.com>
2023-02-09 23:36:37 +0900

    [TLE]  recursive -> for-loop for dfs and assign_chain_top and succeed until 4% (small improvement)

--- baekjoon/국제메시기구.py
+++ baekjoon/국제메시기구.py
@@ -5,31 +5,46 @@ sys.setrecursionlimit(100000)
 MOD = 2**32
 
 
-def dfs(curr, parent):
-    hld["parent"][curr] = parent
-    hld["size"][curr] = 1
-    for v in adjacency_list[curr]:
-        if v != parent:
-            dfs(v, curr)
-            hld["size"][curr] += hld["size"][v]
+def dfs(root):
+    stack = [root]
+    marked = set([root])
+    while stack:
+        node = stack.pop()
+        list_neighbors = [ne for ne in adjacency_list[node] if ne not in marked]
+        if len(list_neighbors)==0:
+            for ne in adjacency_list[node]:
+                if ne != hld["parent"][node]:
+                    hld["size"][node] += hld["size"][ne]
+        else:
+            stack.append(node) 
+            for ne in list_neighbors:
+                hld["parent"][ne] = node
+                stack.append(ne)
+                marked.add(ne)
 
 
-def _assign_chain_top(curr, parent, chain_top):
-    global count
-    pos_seg[curr] = count
-    count += 1
+def assign_chain_top(root):
+    step = 0
+    stack = [(root, root, root)]
+    visited = set([])
+    while stack:
+        curr, parent, chain_top = stack.pop()
+        if curr in visited:
+            pos_seg_end[curr] = step - 1
+        else:
+            stack.append((curr, parent, chain_top))
+            visited.add(curr)
+            pos_seg[curr] = step
+            hld["chain_top"][curr] = chain_top
+            step += 1
 
-    hld["chain_top"][curr] = chain_top
-    list_child = [v for v in adjacency_list[curr] if v != parent]
-    if len(list_child) > 0:  # intermediate node
-        node_heavy_edge = max(list_child, key=lambda x: hld["size"][x])
-        # remain top for heavy edge
-        _assign_chain_top(node_heavy_edge, curr, chain_top)
-        for v in adjacency_list[curr]:
-            if v != parent and v != node_heavy_edge:
-                _assign_chain_top(v, curr, v)  # light edge (new top)
-
-    pos_seg_end[curr] = count-1
+        list_child = [v for v in adjacency_list[curr] if v != parent and v not in visited]
+        if len(list_child) > 0:
+            node_heavy_edge = max(list_child, key=lambda x: hld["size"][x])
+            for v in adjacency_list[curr]:
+                if v != parent and v != node_heavy_edge:
+                    stack.append((v, curr, v))
+            stack.append((node_heavy_edge, curr, chain_top))
 
 
 def propagate(node_index, node_s, node_e):
@@ -157,14 +172,14 @@ for _ in range(N-1):
 queries = [sys.stdin.readline().rstrip().split() for _ in range(Q)]
 
 n_nodes = len(adjacency_list)
-hld = {"size": [None] * n_nodes, "parent": [None]
-        * n_nodes, "chain_top": [None] * n_nodes}
+hld = {"size": [1] * n_nodes, "parent": [0]
+        * n_nodes, "chain_top": [0] * n_nodes}
 
-dfs(0, 0)
+dfs(0)
 count = 0
 pos_seg = [0] * n_nodes
 pos_seg_end = [0] * n_nodes
-_assign_chain_top(0, 0, 0)
+assign_chain_top(0)
 
 pow2 = 1
 while pow2 < N:
