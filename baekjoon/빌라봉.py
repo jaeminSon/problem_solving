3dab7d041321f2862e2c2a6906204560e971a9dc
jaeminSon <woalsdnd@gmail.com>
2023-07-23 20:30:58 +0900

    answer right (checked with IoI official site dataset) but slow for boj

--- baekjoon/빌라봉.py
+++ baekjoon/빌라봉.py
@@ -1,42 +1,20 @@
 from collections import defaultdict
 
 
-def retrieve_connected_trees(graph, n_nodes):
-
-    def dfs(graph, root):
-        stack = [root]
-        marked = set([root])
-        preorder = []
-        while stack:
-            node = stack.pop()
-            preorder.append(node)
-            for ne in graph[node]:
-                if ne not in marked:
-                    stack.append(ne)
-                    marked.add(ne)
-        return preorder
-
-    list_roots = []
-    not_marked = set(range(n_nodes))
-    while len(not_marked) > 0:
-        root = not_marked.pop()
-        nodes = dfs(graph, root)
-        list_roots.append(nodes[0]) # root node
-        not_marked -= set(nodes)
-    return list_roots
-
-
 def diameter(graph, root):
     """
     graph: defaultdict(dict) (graph[u][v] == graph[v][u] == weight)
     """
-    def dfs(graph, root):
+    def dfs(graph, root, track_nodes=False):
         stack = [(root, 0)]
         marked = set([root])
         max_dist = 0
         max_dist_node = root
+        nodes = []
         while stack:
             node, dist = stack.pop()
+            if track_nodes:
+                nodes.append(node)
 
             if dist > max_dist:
                 max_dist_node = node
@@ -47,11 +25,11 @@ def diameter(graph, root):
                     stack.append((ne, dist+w))
                     marked.add(ne)
 
-        return max_dist, max_dist_node
+        return max_dist, max_dist_node, nodes
 
-    _, max_dist_node = dfs(graph, root)
-    max_dist, _ = dfs(graph, max_dist_node)
-    return max_dist
+    _, max_dist_node, _ = dfs(graph, root)
+    max_dist, _, nodes = dfs(graph, max_dist_node, track_nodes=True)
+    return max_dist, nodes
 
 
 def radius(graph, root):
@@ -117,9 +95,18 @@ for _ in range(M):
     a, b, t = map(int, input().split())
     graph[a][b] = graph[b][a] = t
 
-list_roots = retrieve_connected_trees(graph, N)
 
-max_diameter = max([diameter(graph, root) for root in list_roots], default=0)
+list_roots = []
+max_diameter = 0
+not_marked = set(range(N))
+while len(not_marked) > 0:
+    root = not_marked.pop()
+    max_dist, nodes = diameter(graph, root)
+    if len(nodes) > 0:
+        max_diameter = max(max_diameter, max_dist)
+    list_roots.append(nodes[0])  # root node
+    not_marked -= set(nodes)
+
 list_radius = [radius(graph, tree) for tree in list_roots]
 
 if len(list_roots) == 0:
@@ -129,5 +116,16 @@ elif len(list_roots) == 1:
 elif len(list_roots) == 2:
     print(max(max_diameter, sum(list_radius) + L))
 else:
-    list_radius = sorted(list_radius)
-    print(max(max_diameter, sum(list_radius[-2:])+L, sum(list_radius[-3:-1])+2*L))
+    ans1, ans2, ans3 = 0, 0, 0
+    for r in list_radius:
+        if ans1 <= r:
+            ans3 = ans2
+            ans2 = ans1
+            ans1 = r
+        elif ans2 <= r < ans1:
+            ans3 = ans2
+            ans2 = r
+        elif ans3 <= r:
+            ans3 = r
+
+    print(max(max_diameter, ans1+ans2+L, ans2 + ans3+2*L))
