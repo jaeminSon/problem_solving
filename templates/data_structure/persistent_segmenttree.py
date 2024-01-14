8115fe70923b1da4f981ad9fe95666b320961854
jaeminSon <woalsdnd@gmail.com>
2024-01-14 20:23:34 +0900

    add data_type

--- templates/data_structure/persistent_segmenttree.py
+++ templates/data_structure/persistent_segmenttree.py
@@ -1,5 +1,10 @@
 from collections import defaultdict
 
+import sys
+sys.path.append("..")
+from custom_type import TREE
+
+
 MAX_N = 100_001
 
 def just_bigger_power_2(val):
@@ -12,7 +17,7 @@ def generate_tree(tree_size):
     # node: [l, r, value]
     return [[0,0,0]]+ [[i<<1, i<<1|1, 0] for i in range(1, tree_size // 2)] + [[0,0,0] for _ in range(tree_size // 2)]  # dummy first element
 
-def update(tree, node, s, e, increment, index):
+def update(tree:TREE, node, s, e, increment, index):
     tree[node][2] += increment
     if s != e: 
         mid = (s + e) // 2
@@ -26,7 +31,7 @@ def update(tree, node, s, e, increment, index):
             tree.append([tree[right_ch][0], tree[right_ch][1], tree[right_ch][2]])
             update(tree, tree[node][1], mid + 1, e, increment, index)
 
-def query(tree, node, s, e, l, r):
+def query(tree:TREE, node, s, e, l, r):
     # [l, r]
     if r < s or l > e:
         return 0
@@ -37,7 +42,7 @@ def query(tree, node, s, e, l, r):
         left_ch, right_ch, val = tree[node]
         return query(tree, left_ch, s, mid, l, r) + query(tree, right_ch, mid + 1, e, l, r)
 
-def get_kth(tree, next_node, prev_node, s, e, k):
+def get_kth(tree:TREE, next_node, prev_node, s, e, k):
     if s == e:
         return s
     else:
