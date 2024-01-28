83bf72fda77a2875cdef1a7531470c0aa66e8bab
jaeminSon <woalsdnd@gmail.com>
2024-01-28 23:13:30 +0900

    add convex_hull_trick_lichao_tree

--- templates/optimization/dynamic_programming.py
+++ templates/optimization/dynamic_programming.py
@@ -65,7 +65,8 @@ def bitonic_traveling_salesman(distance_matrix: LIST2D) -> REAL:
 def convex_hull_trick(A: LIST1D, B: LIST1D, C: LIST1D, D: LIST1D) -> REAL:
     # dp[i] = min(0 ≤ j < i){A[i]B[j] + dp[j]+ C[j]} + D[i]
     # assert dp[0] == 0 (null state)
-    # constraint: B should be monotonically decreasing => O(nlogn)
+    # no constraint: O(nlogn) with lichao tree
+    # constraint: B should be monotonically decreasing => O(nlogn) with binary search
     #             (additionally) if A is non-decreasing => O(n)
     # line segment formula: f(x) = B[j] * x + dp[j]+ C[j] (A[i] ~ x)
 
@@ -113,6 +114,90 @@ def convex_hull_trick(A: LIST1D, B: LIST1D, C: LIST1D, D: LIST1D) -> REAL:
 
         if D:
             dp[j] += D[j]
+
+    return dp[n-1]
+
+
+def convex_hull_trick_lichao_tree(A, B, C, D):
+
+    assert len(A) == len(B)
+    if C:
+        assert len(A) == len(C)
+    if D:
+        assert len(A) == len(D)
+
+    class Line(object):
+        def __init__(self, slope, intercept):
+            self.slope = slope
+            self.intercept = intercept
+
+        def eval(self, x):
+            return self.slope * x + self.intercept
+
+    class Node(object):
+        def __init__(self, line, s, e, L=None, R=None):
+            self.line = line
+            self.s = int(s)
+            self.e = int(e)
+            self.L = L
+            self.R = R
+
+    class LiChaoTree(object):
+
+        def __init__(self, s, e):
+            self.tree = [Node(Line(0, -float("inf")), s, e, None, None)]
+
+        def insert(self, new_line: Line, index_node=0):
+
+            s = self.tree[index_node].s
+            e = self.tree[index_node].e
+
+            if self.tree[index_node].line.eval(s) > new_line.eval(s):
+                high = self.tree[index_node].line
+                low = new_line
+            else:
+                low = self.tree[index_node].line
+                high = new_line
+
+            if low.eval(e) <= high.eval(e):
+                self.tree[index_node].line = high
+            else:
+                m = s + e >> 1
+                if low.eval(m) < high.eval(m):
+                    self.tree[index_node].line = high
+                    if self.tree[index_node].R is None:
+                        self.tree[index_node].R = len(self.tree)
+                        self.tree.append(
+                            Node(Line(0, -float("inf")), m, e, None, None))
+                    self.insert(low, self.tree[index_node].R)
+                else:
+                    self.tree[index_node].line = low
+                    if self.tree[index_node].L is None:
+                        self.tree[index_node].L = len(self.tree)
+                        self.tree.append(
+                            Node(Line(0, -float("inf")), s, m, None, None))
+                    self.insert(high, self.tree[index_node].L)
+
+        def query(self, x, index_node=0):
+            if index_node is None:
+                return -float("inf")
+            else:
+                s = self.tree[index_node].s
+                e = self.tree[index_node].e
+                m = s + e >> 1
+                if x <= m:
+                    return max(self.tree[index_node].line.eval(x), self.query(x, self.tree[index_node].L))
+                else:
+                    return max(self.tree[index_node].line.eval(x), self.query(x, self.tree[index_node].R))
+
+    lichao = LiChaoTree(0, 1e9)
+
+    n = len(A)
+    dp = [0] * n
+    for j in range(1, n):
+        intercept = dp[j-1] + C[j-1]
+        lichao.insert(Line(-B[j-1], -intercept))
+        dp[j] = -lichao.query(A[j])
 
     return dp[n-1]
 
