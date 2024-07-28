504a4eef11014c954c6fc793ddf2df2efa77093f
jaeminSon <woalsdnd@gmail.com>
2024-07-28 23:35:58 +0900

    add snippets for linked_list

--- templates/misc/linked_list.py
+++ templates/misc/linked_list.py
@@ -1,13 +1,23 @@
+class BidirectionalLinkedList:
+    def build_bidirectional_linked_list(self, arr):
+        self.left = [0] * len(arr)
+        self.right = [0] * len(arr)
+        for i in len(arr):
+            self.left[i] = i-1
+            self.right[i] = i+1
+    
+    def remove(self, l, r):
+        # remove l and r from the linked list
+        self.right[self.left[l]] = self.right[r]
+        self.left[self.right[r]] = self.left[l]
 
-arr = [] # some list
-
-# build_bidirectional_linked_list
-left = [0] * len(arr)
-right = [0] * len(arr)
-for i in len(arr):
-    left[i] = i-1
-    right[i] = i+1
-
-# remove l and r from the linked list
-right[left[l]] = right[r]
-left[right[r]] = left[l]
+def index_prev_occurrence(arr):
+    prev = []
+    v2i = {}
+    for i, v in enumerate(arr):
+        if v in v2i:
+            prev.append(v2i[v])
+        else:
+            prev.append(-1)
+        v2i[v] = i
+    return prev
